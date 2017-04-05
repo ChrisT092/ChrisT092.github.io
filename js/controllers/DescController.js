342db3aa7ac077c641dab2aace9aea805da1d910
ChrisT092 <christophelam017@gmail.com>
2017-04-06 00:15:10 +0200

    Video change
    Use <div> to add video

--- js/controllers/DescController.js
+++ js/controllers/DescController.js
@@ -1,7 +1,13 @@
 app.controller('DescController', ['$scope', 'streamers', '$routeParams', function($scope, streamers, $routeParams) {
   streamers.success(function(data) {
 	$scope.detail = data.streams[$routeParams.id];
-	document.getElementById("videoTwitch").setAttribute("src", "http://player.twitch.tv/?channel=" + $scope.detail.channel.name + "&muted=true");
 	document.getElementById("titre").innerHTML = $scope.detail.channel.display_name;
+	var options = {
+		width: 1200,
+		height: 700,
+ 		channel: $scope.detail.channel.name,
+	};
+	var player = new Twitch.Player("videoTwitch", options);
+	player.setVolume(0);
   });
 }]);
